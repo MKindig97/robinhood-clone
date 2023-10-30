f72f4826d09a686f2b937ee773b4c638033ee5d4
Matt Kindig <mkindig97@gmail.com>
2023-10-30 14:20:22 -0400

    Created Navigation Bar.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div>
+      <div className="app__header">
+        <Header />
+      </div>
+      <div className="app__body"></div>
+    </div>
+  );
+}
+
+export default App;
